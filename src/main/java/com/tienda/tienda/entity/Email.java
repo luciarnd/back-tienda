a6f5a1271da7209f700320e32609bf740614fcd1
HIBERUS\arodriguezh <arodriguezh@hiberus.com>
2022-05-25 11:54:44 +0200

    detalle pedidos

--- src/main/java/com/tienda/tienda/entity/Email.java
+++ src/main/java/com/tienda/tienda/entity/Email.java
@@ -16,6 +16,30 @@ public class Email {
 	private String asunto;
 	
 	private String Mensaje;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getMensaje() {
+		return Mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		Mensaje = mensaje;
+	}
 	
 
 
